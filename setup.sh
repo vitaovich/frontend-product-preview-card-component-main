7ff372a269c8c9d3aa6669d231804a9ea02bc65f
vitaovich <vitaliyalekhnovich@gmail.com>
2023-02-16 18:47:20 +0000

    setup initial

--- setup.sh
+++ setup.sh
@@ -15,19 +15,19 @@ git push
 
 # az login
 
-# if [ $(az group exists --name $resourceGroup) = false ]; then 
-#    az group create --name $resourceGroup --location "$location" 
-# else
-#    echo $resourceGroup
-# fi
+if [ $(az group exists --name $resourceGroup) = false ]; then 
+   az group create --name $resourceGroup --location "$location" 
+else
+   echo $resourceGroup
+fi
 
-# if [[ $(az staticwebapp list --resource-group $resourceGroup --query "[?name=='$PROJECTDIR'] | length(@)") > 0 ]]
-# then
-#   echo "Static Web App for $PROJECTDIR exists"
-# else
-#   echo "Static Web App for $PROJECTDIR doesn't exist"
-#   az staticwebapp create -n $PROJECTDIR -g $resourceGroup -s $GITURL -l WestUs2 -b main --login-with-github
-# fi
-# az staticwebapp create -n StarterNextJSTailwindCSS -g StarterNextJS -s https://github.com/<YOUR_GITHUB_USERNAME>/starter-nextjs-tailwindcss -l WestUs2 -b main --login-with-github
+if [[ $(az staticwebapp list --resource-group $resourceGroup --query "[?name=='$PROJECTDIR'] | length(@)") > 0 ]]
+then
+  echo "Static Web App for $PROJECTDIR exists"
+else
+  echo "Static Web App for $PROJECTDIR doesn't exist"
+  az staticwebapp create -n $PROJECTDIR -g $resourceGroup -s $GITURL -l WestUs2 -b main --login-with-github
+fi
+az staticwebapp create -n StarterNextJSTailwindCSS -g StarterNextJS -s https://github.com/<YOUR_GITHUB_USERNAME>/starter-nextjs-tailwindcss -l WestUs2 -b main --login-with-github
 
-# git pull
+git pull
