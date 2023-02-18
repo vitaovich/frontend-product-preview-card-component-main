fa30a9950e867b83d06eeb99880b243118082d6f
vitaovich <vitaliyalekhnovich@gmail.com>
2023-02-18 05:24:49 +0000

    clean up

--- setup.sh
+++ setup.sh
@@ -25,9 +25,8 @@ if [[ $(az staticwebapp list --resource-group $resourceGroup --query "[?name=='$
 then
   echo "Static Web App for $PROJECTDIR exists"
 else
-  echo "Static Web App for $PROJECTDIR doesn't exist"
+  echo "Static Web App for $PROJECTDIR doesn't exist creating now."
   az staticwebapp create -n $PROJECTDIR -g $resourceGroup -s $GITURL -l WestUs2 -b main --login-with-github
 fi
-# az staticwebapp create -n StarterNextJSTailwindCSS -g StarterNextJS -s https://github.com/<YOUR_GITHUB_USERNAME>/starter-nextjs-tailwindcss -l WestUs2 -b main --login-with-github
 
 git pull
