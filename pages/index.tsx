4d15beafd824c508aaa70efb522278247f74c721
vitaovich <vitaliyalekhnovich@gmail.com>
2023-02-20 19:10:50 +0000

    add main element

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,49 +12,52 @@ export default function Home() {
       <Head>
         <title>Product Preview Card Component</title>
       </Head>
-      <div className='flex items-center justify-center min-h-screen bg-cream'>
-        <div className="flex flex-col m-4 my-10 bg-white rounded-lg md:flex-row md:w-3/4 md:h-1/2 lg:w-1/2">
-          <div className="hidden relative w-1/2 md:block">
-            <Image
-              src={selfImage}
-              alt="image-product-desktop"
-              className="rounded-l-lg"
-              fill
-            />
-          </div>
-          <Image
-            src="/images/image-product-mobile.jpg"
-            alt="image-product-desktop"
-            className="mx-auto rounded-t-lg md:hidden"
-            width={686}
-            height={480}
-          />
-          <div className="flex flex-col p-6 space-y-6 md:p-8 md:w-1/2 md:pt-12">
-            <h2 className='text-darkgrayishblue text-sm font-montserrat tracking-widest'>PERFUME</h2>
-            <h1 className='text-verydarkblue text-4xl font-fraunces'>Gabrielle Essence Eau De Parfum</h1>
-            <p className='text-darkgrayishblue text-lg font-montserrat'>
-              A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.
-            </p>
-            <div className="flex flex-row items-center space-x-10">
-              <p className='text-4xl text-darkcyan font-fraunces'>
-                $149.99
-              </p>
-              <p className='line-through text-darkgrayishblue'>
-                $169.99
-              </p>
-            </div>
-            <button className='flex items-center justify-center py-4 px-8 rounded-md bg-darkcyan space-x-4 hover:bg-hoverdarkcyan'>
+      <main>
+        <div className='flex items-center justify-center min-h-screen bg-cream'>
+          <div className="flex flex-col m-4 my-10 bg-white rounded-lg md:flex-row md:w-3/4 md:h-1/2 lg:w-1/2">
+            <div className="hidden relative w-1/2 md:block">
               <Image
-                alt='cart'
-                src='/images/icon-cart.svg'
-                width={16}
-                height={16}
+                src={selfImage}
+                alt="image-product-desktop"
+                className="rounded-l-lg"
+                fill
               />
-              <span className='text-white text-lg font-montserrat font-bold'>Add to Cart</span>
-            </button>
+            </div>
+            <Image
+              src="/images/image-product-mobile.jpg"
+              alt="image-product-desktop"
+              className="mx-auto rounded-t-lg md:hidden"
+              width={686}
+              height={480}
+            />
+            <div className="flex flex-col p-6 space-y-6 md:p-8 md:w-1/2 md:pt-12">
+              <h2 className='text-darkgrayishblue text-sm font-montserrat tracking-widest'>PERFUME</h2>
+              <h1 className='text-verydarkblue text-4xl font-fraunces'>Gabrielle Essence Eau De Parfum</h1>
+              <p className='text-darkgrayishblue text-lg font-montserrat'>
+                A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.
+              </p>
+              <div className="flex flex-row items-center space-x-10">
+                <p className='text-4xl text-darkcyan font-fraunces'>
+                  $149.99
+                </p>
+                <p className='line-through text-darkgrayishblue'>
+                  $169.99
+                </p>
+              </div>
+              <button className='flex items-center justify-center py-4 px-8 rounded-md bg-darkcyan space-x-4 hover:bg-hoverdarkcyan'>
+                <Image
+                  alt='cart'
+                  src='/images/icon-cart.svg'
+                  width={16}
+                  height={16}
+                />
+                <span className='text-white text-lg font-montserrat font-bold'>Add to Cart</span>
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+      </main>
+
     </>
   )
 }
